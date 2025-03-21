7021804fda02542011eaa4b2b99a32f978fd2bdf
shericWu <shihyu1127@gmail.com>
2025-03-21 22:33:44 +0800

    Update q-table

--- student_agent.py
+++ student_agent.py
@@ -45,10 +45,8 @@ def get_action(obs):
         first_time = False
     # print("target: ", target, target_pos)
     state = get_state(obs, prev_action, target_pos, got_passenger)
-    # print("state: ", state)
     if np.random.randint(20) == 0:
         action = np.random.randint(4)
-        print("random action:", action)
     else:
         action = np.argmax(q_table[state])
     
